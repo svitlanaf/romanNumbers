47ec12a0e56a1debd6b3dd4069077757bfd90b33
Mariia Stastuk and Svitlana Filatova <svitlana@>
2019-04-04 10:12:17 -0700

    add js logic

--- js/scripts.js
+++ js/scripts.js
@@ -2,15 +2,18 @@ $(document).ready(function(){
   $("form#romanNumbers").submit(function(event){
     event.preventDefault();
     var number = parseInt($("input#userInput").val());
-    var arabicNumbers = [50, 10, 5, 1];
-    var romanNumbers = ["L", "X", "V", "I"];
+    var arabicNumbers = [1000, 900, 500, 400, 100, 90, 50,40, 10,9, 5,4, 1];
+    var romanNumbers = ["M", "CM", "D", "CD", "C","XC","L","XL","X","IX" ,"V","IV", "I"];
     var result = "";
-
-    // i =2
-    // res='X'
-    //arabicNumbers =''10
-    //romanNumbers="x"
-    //numer = 27
+    
+    if (number > 3999) {
+      alert ("Please enter number from 0 to 3999")
+    };
+    // i =0
+    // res='Xxxv'
+    //arabicNumbers =''50
+    //romanNumbers="v"
+    //numer = 2
     for (var i=0; i < romanNumbers.length; i++ ){
       if (number<arabicNumbers[i]){
 
