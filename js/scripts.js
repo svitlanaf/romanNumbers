8c4730c2984f3402f0838c4a102b863663853bd3
Svitlana Filatova <svitlana.filatova.gmail.com>
2019-04-05 16:47:42 -0700

    Bug fixes (invalid input)

--- js/scripts.js
+++ js/scripts.js
@@ -1,33 +1,28 @@
-$(document).ready(function(){
-  $("form#romanNumbers").submit(function(event){
+$(document).ready(function() {
+  $("form#romanNumbers").submit(function(event) {
     event.preventDefault();
     var number = parseInt($("input#userInput").val());
-    var arabicNumbers = [1000, 900, 500, 400, 100, 90, 50,40, 10,9, 5,4, 1];
-    var romanNumbers = ["M", "CM", "D", "CD", "C","XC","L","XL","X","IX" ,"V","IV", "I"];
+    var arabicNumbers = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
+    var romanNumbers = ["M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"];
     var result = "";
 
-    if (number > 3999 || ($("input#userInput").val() === "") || (isNaN($("input#userInput").val())) || (/[$-/:-?{-~!"^_`\[\]]/) || ($("input#userInput").val() === 0))  {
-      alert ("Please enter number from 1 to 3999")
+    var input = $("#userInput").val();
+    if (($('input').val() === "")|| number > 3999 || number <=0) {
+      alert("Please enter number from 1 to 3999")
     } else {
-    // i =0
-    // res='Xxxv'
-    //arabicNumbers =''50
-    //romanNumbers="v"
-    //numer = 2
-    for (var i=0; i < romanNumbers.length; i++ ){
-      if (number<arabicNumbers[i]){
+      for (var i = 0; i < romanNumbers.length; i++) {
+        if (number < arabicNumbers[i]) {
 
-      }else{
-      number = number - arabicNumbers[i];
-        result+= romanNumbers[i];
-        i = i-1;
+        } else {
+          number = number - arabicNumbers[i];
+          result += romanNumbers[i];
+          i = i - 1;
+        }
+      }
 
+      $(".output").text(result);
+      $("#result").show();
     }
-}
-
-    $(".output").text(result);
-    $("#result").show();
-}
   });
 
 });
